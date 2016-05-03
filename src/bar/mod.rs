080d487ce93a19a9799eba00e57f4f0ad7d76751
Getty Ritter <gdritter@galois.com>
2016-05-03 15:00:30 -0700

    being a tiny bit nitpicky about indentatio

--- src/bar/mod.rs
+++ src/bar/mod.rs
@@ -4,8 +4,8 @@ pub const NAME: &'static str = "bar/mod.rs";
 
 pub fn print_helper() {
     println!("in bar/mod.rs {{");
-    println!("        foo::NAME\t= {:?}", foo::NAME);
+    println!("        foo::NAME\t= {:?}",       foo::NAME);
     println!("  self::foo::NAME\t= {:?}", self::foo::NAME);
-    println!("      ::foo::NAME\t= {:?}", ::foo::NAME);
+    println!("      ::foo::NAME\t= {:?}",     ::foo::NAME);
     println!("}}");
 }
