c63bfd4e202adc9cc4d034c70c9b6b8a49d2c075
Getty Ritter <gdritter@galois.com>
2016-05-03 15:01:50 -0700

    More symmetrical

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod bar;
 
 fn main() {
     println!("in main.rs {{");
-    println!("    foo::NAME\t\t= {:?}", foo::NAME);
-    println!("    bar::NAME\t\t= {:?}", bar::NAME);
-    println!("    bar::foo::NAME\t= {:?}", bar::foo::NAME);
+    println!("        foo::NAME\t= {:?}",       foo::NAME);
+    println!("  self::foo::NAME\t= {:?}", self::foo::NAME);
+    println!("      ::foo::NAME\t= {:?}",     ::foo::NAME);
     println!("}}");
     bar::print_helper();
 }
